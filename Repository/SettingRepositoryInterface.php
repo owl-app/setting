47ff0d895a8942dce17b208c9fb4f6d9d0682d0c
owl-app <owl@pawelkeska.eu>
2023-09-05 14:01:23 +0000

    Fixes psalm

--- Repository/SettingRepositoryInterface.php
+++ Repository/SettingRepositoryInterface.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace Owl\Component\Setting\Repository;
 
 use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Owl\Component\Setting\Model\SettingInterface;
 
+/**
+ * @template T of SettingInterface
+ *
+ * @extends RepositoryInterface<T>
+ */
 interface SettingRepositoryInterface extends RepositoryInterface
 {
     public function finAllBySection(string $section): array;
